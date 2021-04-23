e558a2977ddd3986413c91ca10533b8bc0fa2151
Eros Fabrici <eros.fabrici@gmail.com>
2021-04-23 12:45:34 +0200

    create accounts app and user model

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from . import forms
+
 
 # Create your views here.
+
+class SignUp(CreateView):
+    form_class = forms.UserCreationForm
+    # once someone has signed up successfully, we redirect there when clicking on submit
+    success_url = reverse_lazy('login')
+    template_name = 'accounts/signup.html'
