c5255ce6959ca5c0e9313ba505b2b520f7a61940
Eros Fabrici <eros.fabrici@gmail.com>
2021-04-23 17:06:52 +0200

    added url mappings and registration

--- groups/admin.py
+++ groups/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from . import models
+
 
 # Register your models here.
+
+# allows us to utilise the admin interface with the ability to modify
+# models on the same page as the parent model
+class GroupMemberInline(admin.TabularInline):
+    models = models.GroupMember
+
+
+admin.site.register(models.Group)
