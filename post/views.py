f941e22b08c6e8ffdbd47155f924adb08e28d5bd
eferos93 <eros.fabrici@gmail.com>
2021-04-24 17:00:24 +0200

    finished

--- post/views.py
+++ post/views.py
@@ -22,26 +22,26 @@ class PostList(SelectRelatedMixin, generic.ListView):
 
 class UserPosts(generic.ListView):
     model = models.Post
-    template_name = '../post/templates/post/user_post_list.html'
+    template_name = 'post/user_post_list.html'
 
     def get_queryset(self):
         try:
-            self.post.user = User.objects \
+            self.model.user = User.objects \
                 .prefetch_related('posts') \
                 .get(username__iexact=self.kwargs.get('username'))
         except User.DoesNotExist:
             raise Http404
         else:
-            return self.post_user.posts.all()
+            return self.model.user.posts.all()
 
     def get_context_data(self, *args, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['post_user'] = self.post_user
+        context['post_user'] = self.model.user
         return context
 
 
 class PostDetail(SelectRelatedMixin, generic.DetailView):
-    models = models.Post
+    model = models.Post
     select_related = ('user', 'group')
 
     def get_queryset(self):
@@ -58,7 +58,7 @@ class CreatePost(generic.CreateView, LoginRequiredMixin, SelectRelatedMixin):
         self.object = form.save(commit=False)
         self.object.user = self.request.user
         self.object.save()
-        return super().form_valid()
+        return super().form_valid(form)
 
 
 class DeletePost(LoginRequiredMixin, SelectRelatedMixin, generic.DeleteView):
